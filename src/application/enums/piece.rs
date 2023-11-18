22d8bc3f2b07c5bf873cd2a8d5d1c2aa8294237f
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2023-11-18 11:47:42 +0300

    Отображение игровой статистики

--- src/application/enums/piece.rs
+++ src/application/enums/piece.rs
@@ -5,3 +5,21 @@ pub enum Piece {
     Man,
     King,
 }
+
+impl Piece {
+    pub fn is_man(&self) -> bool {
+        if let Piece::Man = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_king(&self) -> bool {
+        if let Piece::King = self {
+            true
+        } else {
+            false
+        }
+    }
+}
